6fdf6731455fdd283d34c9bc1af8f58bea89ba03
qiesuijifengqifei <164482645+qiesuijifengqifei@users.noreply.github.com>
2024-04-04 18:28:38 +0000

    update devcontainer.json

--- .devcontainer/env.sh
+++ .devcontainer/env.sh
@@ -1,2 +1,3 @@
+echo "source /etc/bash_completion" >> ~/.bashrc
 echo "alias frontend='cd /workspaces/userlogin/frontend/userlogin'" > ~/.bash_aliases
 echo "alias backend='cd /workspaces/userlogin/backend/userlogin'" >> ~/.bash_aliases
