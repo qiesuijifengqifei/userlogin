c320afd40a65cfe9317c57cbbe8f79f9112dbaf7
qiesuijifengqifei <164482645+qiesuijifengqifei@users.noreply.github.com>
2024-09-18 08:30:51 +0000

    Optimize login information

--- frontend/userlogin/src/components/UserLogin.vue
+++ frontend/userlogin/src/components/UserLogin.vue
@@ -2,21 +2,36 @@
 import { ElMessage } from 'element-plus';
 import { useUserStore } from '../stores/user.ts';
 import { request } from '../utils/request.ts'
+import { ref, reactive } from 'vue'
+import { User, Lock } from '@element-plus/icons-vue';
 
-let rturl = "";
-if ( window.location.href.indexOf("?") != -1 ){
-  rturl = window.location.href.split("?")[1].split("=")[1]
-  // console.log(rturl)
-}
+let rturl = window.location.href.indexOf("?") != -1 ? window.location.href.split("?")[1].split("=")[1] : ""
 
 // let current_url = window.location.href.split("//")[1]
 // console.log(process.env)                           // 客户端不能获取到此值
 // console.log(import.meta.env.VITE_PROXY_URL)        // 客户端能获取到此值
 
 const userStore = useUserStore()
+const loginFormData = reactive({
+  username: userStore.userinfo.username,
+  password: userStore.userinfo.password,
+})
+const [usernameError, passwordError] = [ref(''), ref('')]
 const doLogin = async () => {
-  try {    
-    const response = await request.post('/api/login', userStore.userinfo);
+  if (loginFormData.username === "") {
+    return usernameError.value = "用户名不能为空"
+  } else {
+    usernameError.value = ""
+  }
+  if (loginFormData.password === "") {
+    return passwordError.value = "密码不能为空"
+  } else {
+    passwordError.value = ""
+  }
+
+
+  try {
+    const response = await request.post('/api/login', loginFormData);
     // 登录成功处理逻辑，例如保存token等
     if (response.status == 200) {
       ElMessage({
@@ -24,22 +39,20 @@ const doLogin = async () => {
         type: 'success',
         plain: true,
       })
+      userStore.userinfo.username = loginFormData.username
       userStore.userinfo.token = response.data.token
-      if (! userStore.userinfo.rememberme) {
+      if (!userStore.userinfo.rememberme) {
         userStore.userinfo.password = ""
+      } else {
+        userStore.userinfo.password = loginFormData.password
       }
       if (rturl !== "") {
         rturl = rturl + "?token=" + response.data.token
         window.location.replace(rturl);
       }
-      
     }
     else {
-      ElMessage({
-        message: '密码错误',
-        type: 'error',
-        plain: true,
-      })
+      return passwordError.value = "密码错误"
     }
 
 
@@ -56,25 +69,32 @@ const doLogin = async () => {
 </script>
 
 <template>
-  <div>
-    <br />
-    <el-input
-      v-model="userStore.userinfo.username"
-      type="text"
-      placeholder="name"
-    />
-    <br /><br />
-    <el-input
-      v-model="userStore.userinfo.password"
-      type="password"
-      placeholder="password"
-      show-password
-    />
-    <br /><br />
-    <el-checkbox v-model="userStore.userinfo.rememberme" label="记住密码" size="large" />
-    <span>&nbsp &nbsp &nbsp</span>
-    <el-button type="primary" @click="doLogin">Login</el-button>
-  </div>
+  <el-form class="login">
+    <el-form-item :error="usernameError">
+      <el-input v-model="loginFormData.username" type="text" :prefix-icon="User" placeholder="name" maxlength="10" />
+    </el-form-item>
+
+    <el-form-item :error="passwordError">
+      <el-input v-model="loginFormData.password" :prefix-icon="Lock" type="password" placeholder="password"
+        maxlength="16" show-password @keyup.enter.native="doLogin" />
+    </el-form-item>
+
+    <el-form-item>
+      <el-checkbox v-model="userStore.userinfo.rememberme" label="记住密码" size="large" />
+      <el-button type="primary" @click="doLogin"> Login </el-button>
+    </el-form-item>
+
+  </el-form>
+
 </template>
 
-<style scoped></style>
+<style scoped>
+.login {
+  width: 200px
+}
+
+button {
+  right: 0%;
+  position: absolute
+}
+</style>
--- frontend/userlogin/src/main.ts
+++ frontend/userlogin/src/main.ts
@@ -5,11 +5,16 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+// import * as ElementPlusIconsVue from '@element-plus/icons-vue'           // 全局导入 icons 包
 
 const app = createApp(App)
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 app.use(pinia)
 app.use(ElementPlus)
+// for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+//     app.component(key, component)
+// }
+
 
 app.mount('#app')
